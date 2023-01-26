e901b8c246c1e531105dc281c6a5860a93534938
Parker Parham <parkerparham@gmail.com>
2023-01-26 17:03:11 -0500

    Done with assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    result = []
+    if length > 0:
+        result.append(0)
+        if length > 1:
+            result.append(1)
+            if length > 2:
+                i = 2
+                while i < length:
+                    number = result[-2] + result[-1]
+                    result.append(number)
+                    i += 1
+    print(result)
+    
